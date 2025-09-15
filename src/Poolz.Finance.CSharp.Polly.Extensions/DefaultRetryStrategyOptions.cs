30318cbd2f44b1fadce502c8abd3da2e7494657d
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2025-09-15 20:00:13 +0300

    Project initial commit (#2)

---
+++ src/Poolz.Finance.CSharp.Polly.Extensions/DefaultRetryStrategyOptions.cs
@@ -0,0 +1,19 @@
+﻿using Polly;
+using System;
+using Polly.Retry;
+
+namespace Poolz.Finance.CSharp.Polly.Extensions
+{
+    public class DefaultRetryStrategyOptions<TResult> : RetryStrategyOptions<TResult>
+    {
+        public DefaultRetryStrategyOptions()
+        {
+            Name = "Retry";
+            MaxRetryAttempts = 2;
+            BackoffType = DelayBackoffType.Constant;
+            UseJitter = false;
+            Delay = TimeSpan.FromMilliseconds(250);
+            ShouldHandle = new PredicateBuilder<TResult>().Handle<Exception>(_ => true);
+        }
+    }
+}
