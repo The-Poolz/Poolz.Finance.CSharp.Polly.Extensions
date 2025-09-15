0b3ba249400c4c8191eaf608b80623ca962ef87a
ArdenHide <ardenhide@gmail.com>
2025-09-15 19:55:21 +0300

    Project initial commit

---
+++ src/Poolz.Finance.CSharp.Polly.Extensions/IRetryExecutor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Polly.Retry;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Poolz.Finance.CSharp.Polly.Extensions
+{
+    public interface IRetryExecutor
+    {
+        public TResult Execute<TResult>(Func<CancellationToken, Task<TResult>> action, RetryStrategyOptions<TResult>? options = null, CancellationToken ct = default);
+        public Task<TResult> ExecuteAsync<TResult>(Func<CancellationToken, Task<TResult>> action, RetryStrategyOptions<TResult>? options = null, CancellationToken ct = default);
+    }
+}
---
+++ src/Poolz.Finance.CSharp.Polly.Extensions/RetryExecutor.cs
@@ -0,0 +1,27 @@
+﻿using Polly;
+using System;
+using Polly.Retry;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Poolz.Finance.CSharp.Polly.Extensions
+{
+    public class RetryExecutor : IRetryExecutor
+    {
+        public TResult Execute<TResult>(Func<CancellationToken, Task<TResult>> action, RetryStrategyOptions<TResult>? options = null, CancellationToken ct = default)
+        {
+            return ExecuteAsync(action, options, ct).GetAwaiter().GetResult();
+        }
+
+        public async Task<TResult> ExecuteAsync<TResult>(Func<CancellationToken, Task<TResult>> action, RetryStrategyOptions<TResult>? options = null, CancellationToken ct = default)
+        {
+            options ??= new DefaultRetryStrategyOptions<TResult>();
+
+            var pipeline = new ResiliencePipelineBuilder<TResult>()
+                .AddRetry(options)
+                .Build();
+
+            return await pipeline.ExecuteAsync(token => new ValueTask<TResult>(action(token)), ct);
+        }
+    }
+}
