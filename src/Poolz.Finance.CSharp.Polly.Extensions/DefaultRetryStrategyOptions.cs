7a6753324c20ff159af1750cea7a261bf502ae7f
ArdenHide <ardenhide@gmail.com>
2025-09-16 12:38:01 +0300

    Include logging

--- src/Poolz.Finance.CSharp.Polly.Extensions/DefaultRetryStrategyOptions.cs
+++ src/Poolz.Finance.CSharp.Polly.Extensions/DefaultRetryStrategyOptions.cs
@@ -6,7 +6,7 @@ namespace Poolz.Finance.CSharp.Polly.Extensions
 {
     public class DefaultRetryStrategyOptions<TResult> : RetryStrategyOptions<TResult>
     {
-        public DefaultRetryStrategyOptions()
+        public DefaultRetryStrategyOptions(Action<string>? log = null, Func<OnRetryArguments<TResult>, string>? format = null)
         {
             Name = "Retry";
             MaxRetryAttempts = 2;
@@ -14,6 +14,16 @@ namespace Poolz.Finance.CSharp.Polly.Extensions
             UseJitter = false;
             Delay = TimeSpan.FromMilliseconds(250);
             ShouldHandle = new PredicateBuilder<TResult>().Handle<Exception>(exception => !(exception is OperationCanceledException));
+            OnRetry = args =>
+            {
+                var ex = args.Outcome.Exception;
+                if (ex == null || log == null) return default;
+
+                var message = format?.Invoke(args) ?? $"[Retry] Attempt={args.AttemptNumber}, Delay={args.RetryDelay}, Exception={ex.GetType().Name}: {ex.Message}";
+                log(message);
+
+                return default;
+            };
         }
     }
 }
